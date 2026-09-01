ddde4fe705c0d8da6fa6a15287635764c1d14682
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-09-01 10:18:35 +0530

    feat: implement user registration, retrieval, and token refresh functionality

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,82 @@
+import usermodel from "../models/user.model.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import {ENV} from "../lib/env.js"
+
+export const registerUser = async(req,res)=>{
+      const {username,email,password}=req.body;
+
+      const isAlreadyRegistered = await usermodel.findOne({$or:[ {username},{email}]});
+
+      if(isAlreadyRegistered){
+            return res.status(409).json({message:"User already registered"});
+      }
+
+      const hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
+
+      const newUser = await new usermodel({
+        username,
+        email,
+        password:hashedPassword
+      });
+
+      await newUser.save();
+
+    const accessToken = jwt.sign({id:newUser._id},ENV.JWT_SECRET,{expiresIn:"15m"});
+
+    const refreshToken = jwt.sign({id:newUser._id},ENV.JWT_SECRET,{expiresIn:"7d"});
+
+   res.cookie("refreshToken",refreshToken,{
+    httpOnly:true,
+    secure:true,
+    sameSite:"strict",
+    maxAge:7*24*60*60*1000 
+   })
+
+
+    res.status(201).json({message:"User created Sucessfully",user:{username:newUser.username,email:newUser.email},accessToken
+    }
+    )
+}
+
+export const getMe = async(req,res)=>{
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if(!token)
+  {
+    return res.status(401).json({message:"token not found"})
+  }
+
+  const decoded =jwt.verify(token , ENV.JWT_SECRET);
+  
+  const user = await usermodel.findById(decoded.id).select("-password")
+
+  res.status(200).json({message:"user fetched sucessfully",user:{username:user.username,email:user.email}})
+
+
+}
+
+export const refreshToken = async(req,res)=> {
+
+  const refreshToken = req.cookies.refreshToken;
+
+  if(!refreshToken){
+    return res.status(401).json({message:"refresh token not found"})
+  }
+
+  const decoded = jwt.verify(refreshToken, ENV.JWT_SECRET);
+
+  const accessToken = jwt.sign({id:decoded.id},ENV.JWT_SECRET,{expiresIn:"15m"});
+
+  const newRefreshToken = jwt.sign({id:decoded.id},ENV.JWT_SECRET,{expiresIn:"7d"});
+
+  res.cookie("refreshToken", newRefreshToken,{
+    httpOnly:true,
+    secure:true,
+    sameSite:"strict",
+    maxAge:7*24*60*60*1000
+  });
+
+  res.status(200).json({message:"Access token refreshed sucessfully",accessToken});
+
+}
