152cb9841b0c38089fe23b15430d5e23aa379585
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-09-03 19:18:08 +0530

    feat: add missing environment variables for Google API integration

--- src/lib/env.js
+++ src/lib/env.js
@@ -4,7 +4,12 @@ dotenv.config({ quiet: true });
 export const ENV=({
     PORT:process.env.PORT,
     DB_URL:process.env.DB_URL,
-    JWT_SECRET:process.env.JWT_SECRET
+    JWT_SECRET:process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
+    GOOGLE_USER: process.env.GOOGLE_USER
+    
 });
 
 if(!ENV.DB_URL){
@@ -15,4 +20,18 @@ if(!ENV.JWT_SECRET){
    throw new Error("JWT_SECRET is not defined in the environment variable"); 
         }    
 
+if (!ENV.GOOGLE_CLIENT_ID) {
+    throw new Error("GOOGLE_CLIENT_ID is not defined in environment variables");
+}
 
+if (!ENV.GOOGLE_CLIENT_SECRET) {
+    throw new Error("GOOGLE_CLIENT_SECRET is not defined in environment variables");
+}
+
+if (!ENV.GOOGLE_REFRESH_TOKEN) {
+    throw new Error("GOOGLE_REFRESH_TOKEN is not defined in environment variables");
+}
+
+if (!ENV.GOOGLE_USER) {
+    throw new Error("GOOGLE_USER is not defined in environment variables");
+}
