e108d1b38ccd0f788e529d692aa7ca662c53cb0f
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-09-04 23:35:46 +0530

    add email server verification to nodemailer transport

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -12,4 +12,11 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+transporter.verify((error, success) => {
+    if (error) {
+        console.error('Error connecting to email server:', error);
+    } else {
+        console.log('Email server is ready to send messages');
+    }
+});
 
