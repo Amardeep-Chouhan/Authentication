c8d6ed4b535f339063be97ce7dfac012410e1a6a
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-09-04 23:36:45 +0530

    implement sendEmail function for sending emails via nodemailer

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -19,4 +19,19 @@ transporter.verify((error, success) => {
         console.log('Email server is ready to send messages');
     }
 });
+export const sendEmail = async (to, subject, text, html) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `"Your Name" <${ENV.GOOGLE_USER}>`, 
+            to, 
+            subject, 
+            text, 
+            html, 
+        });
 
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
