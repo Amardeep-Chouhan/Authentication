f107746bfbd6dc7f04d3181e83e7b468508b73df
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-09-05 10:58:13 +0530

    refactor authentication routes to apply rate limiting for register, login, refresh-token, and verify-email endpoints

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,21 @@
-import {Router} from "express";
-import * as authcontroller from "../controllers/auth.controller.js"
+import { Router } from "express";
+import * as authcontroller from "../controllers/auth.controller.js";
+import { authLimiter, otpLimiter } from "../middleware/rateLimiter.js";
 
 const authRouter = Router();
 
+authRouter.post("/register", authLimiter, authcontroller.registerUser);
 
+authRouter.post("/login", authLimiter, authcontroller.login);
 
-authRouter.post("/register",authcontroller.registerUser);
-authRouter.post("/login", authcontroller.login);
 authRouter.get("/get-me", authcontroller.getMe);
-authRouter.get("/refresh-token", authcontroller.refreshToken);
-authRouter.get("/logout", authcontroller.logout);
-authRouter.get("/logout-all", authcontroller.logoutall);
-authRouter.post("/verify-email", authcontroller.verifyEmail);
 
+authRouter.get("/refresh-token", authLimiter, authcontroller.refreshToken);
+
+authRouter.get("/logout", authcontroller.logout);
+
+authRouter.get("/logout-all", authcontroller.logoutall);
+
+authRouter.post("/verify-email", otpLimiter, authcontroller.verifyEmail);
 
 export default authRouter;
